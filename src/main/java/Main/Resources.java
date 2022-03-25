bc6dfbaf7a56aa0bbe4aa26a96a12248e6745440
shrey-deloitte <shredeshpande@deloitte.com>
2022-03-25 12:36:41 +0530

    hardcoded

--- src/main/java/Main/Resources.java
+++ src/main/java/Main/Resources.java
@@ -1,5 +1,7 @@
 package Main;
 
+import org.apache.poi.hssf.record.aggregates.RowRecordsAggregate;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -10,8 +12,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
@@ -20,64 +24,66 @@ import static java.lang.Thread.sleep;
 public class Resources {
 
     static WebDriver driver;
-    public static void launch(){
+
+    public static void launch() {
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("https://www.saucedemo.com/");
     }
 
-    public static void login() throws FileNotFoundException, InterruptedException {
-        driver=new ChromeDriver();
-        String excelFilePath="C:\\Users\\shredeshpande\\Documents\\Ass4UserIDPass.xlsx";
-        FileInputStream fis=new FileInputStream(excelFilePath);
-        XSSFWorkbook  workbook=new XSSFWorkbook();
-        XSSFSheet sheet=workbook.getSheetAt(0);
+    public static void login() throws IOException, InterruptedException {
+        // driver=new ChromeDriver();
+        String filePath = "C:\\Users\\shredeshpande\\Documents\\Ass4UserIDPass.xlsx";
+        File file = new File(filePath);
 
-        XSSFRow row=null;
-        XSSFCell cell=null;
-        String email=null;
-        String password=null;
+        FileInputStream inputStream = new FileInputStream(file);
 
-        for(int i=1;i<sheet.getLastRowNum();i++) {
-            row = sheet.getRow(i);
+        XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+
+        XSSFSheet workbookSheet = workbook.getSheet("Sheet1");
+        int rowCount = workbookSheet.getLastRowNum() - workbookSheet.getFirstRowNum();
+        String email = null;
+        String password = null;
+        for (int i = 1; i < rowCount + 1; i++) {
+
+            Row row = workbookSheet.getRow(i);
             for (int j = 0; j < row.getLastCellNum(); j++) {
-                cell = row.getCell(j);
-                if (j == 0) {
-                    email = cell.getStringCellValue();
-                }
-                if (j == 1) {
-                    password = cell.getStringCellValue();
-                }
+                String data = row.getCell(j).getStringCellValue();
+
+                System.out.println(data);
+
             }
-
-
-            System.out.println("********************************** Reading Data **********************************");
-            driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(email);
-            driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
-
-            sleep(1000);
-            driver.findElement(By.xpath("//input[@id='login-button']")).click();
-            String result = null;
-
-            String actualurl = driver.getCurrentUrl();
-            sleep(3000);
-
-            driver.quit();
         }
 
+        XSSFRow row = null;
+        XSSFCell cell = null;
 
+
+
+        System.out.println("********************************** Reading Data **********************************");
+        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(email);
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
+
+        sleep(1000);
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
+        String result = null;
+
+        //  String actualurl = driver.getCurrentUrl();
+        sleep(3000);
+        System.out.println(email + " " + password);
 
     }
 
-    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\shredeshpande\\Downloads\\chromedriver.exe");
-         driver= new ChromeDriver();
 
+    public static void main(String[] args) throws IOException, InterruptedException ,Exception{
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shredeshpande\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
 
 
         launch();
         login();
     }
-
 }
+
+
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -17,6 +18,15 @@ public class test {
         driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
         driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
         driver.findElement(By.xpath("//input[@id='login-button']")).click();
+
+
+        Select dropdown=new Select(driver.findElement(By.xpath("//select[@class = 'product_sort_container']")));
+        dropdown.selectByVisibleText("Price (high to low)");
+
+        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-fleece-jacket']")).click();
+
+
+
 
 
     }
